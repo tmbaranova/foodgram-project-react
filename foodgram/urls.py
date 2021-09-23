b51e9279b18fc77a6d45542196330f828b152569
Tatyana Baranova <tmb2508@gmail.com>
2021-09-23 08:37:53 +0300

    Delete foodgram directory

--- foodgram/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include ('recipes.urls', namespace='recipes'))
-]
